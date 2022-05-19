6ddbf8fdda9fbd11353be668ce44c2378eaf5b85
Zach Strout <zachstrout@gmail.com>
2022-05-18 22:28:36 -0500

    Create babelrc file

--- setup.bash
+++ setup.bash
@@ -2,5 +2,6 @@
 npm init -y
 npm install --save-dev css-loader eslint eslint-config-airbnb-base eslint-plugin-import html-webpack-plugin prettier style-loader webpack webpack-cli babel-loader @babel/core @babel/preset-env @babel/polyfill jest jest-environment-jsdom
 npm install -g json
+echo '{ "presets": ["@babel/preset-env"] }' > .babelrc
 json -I -f package.json -e "this.scripts.build=\"webpack\""
 json -I -f package.json -e "this.scripts.test=\"jest\""
